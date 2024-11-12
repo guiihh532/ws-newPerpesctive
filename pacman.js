6c3c8c5d34c5312db70032d904262dee159d0941
Guilherme Messias <guiihh532@gmail.com>
2024-11-12 11:59:02 -0300

    plate to move

---
+++ pacman.js
@@ -0,0 +1,41 @@
+class Pacman {
+    constructor(x, y, width, height, speed){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.speed = speed;
+    }
+
+    moveProcess(){
+
+    }
+
+    eat(){
+
+    }
+
+    moveBackWards(){
+
+    }
+
+    moveForWards(){
+
+    }
+
+    checkCollision(){
+
+    }
+
+    checkDirectionIfPossible(){
+
+    }
+
+    changeAnimation(){
+
+    }
+
+    draw(){
+        
+    }
+}
